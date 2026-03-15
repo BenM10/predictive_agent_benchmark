7448146a9f90b0c38808757174def2b154b124fb
BenM10 <benmears616@gmail.com>
2026-03-15 16:36:50 +0000

    Change test size in train/test split from 0.2 to 0.25

--- task/task5_pipeline.py
+++ task/task5_pipeline.py
@@ -16,7 +16,7 @@ X_scaled = scaler.fit_transform(X)
 
 # Train/test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=42
+    X_scaled, y, test_size=0.25, random_state=42
 )
 
 # Train model
